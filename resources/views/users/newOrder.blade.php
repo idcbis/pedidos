814c162fb5a82890c63e83f3ab52c9e79e5183d5
Fabian Toro <helber.toro@gmail.com>
2020-02-10 13:52:39 -0500

    Install sbadmin

---
+++ resources/views/users/newOrder.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.user')
+
+@section('content')
+<div class="row">
+  <div class="col-12 mb-3">
+    <h1>Nuevo pedido</h1>
+  </div>
+</div>
+<div class="row">
+  <div class="col-12">
+    <div class="card border-left-primary shadow">
+      <div class="card-body">
+        <form class="formulario-pedidos" action="">
+          
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/new-order', 'UserController@newOrder')->name('newOrder');
