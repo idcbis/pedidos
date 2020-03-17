010d7c47e3260c4218f05b54f0670bc6a1287843
Fabian Toro <helber.toro@gmail.com>
2020-03-17 11:45:51 -0500

    User - Company relationship

--- app/Company.php
+++ app/Company.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     protected $guarded = [];
+
+    public function users () {
+        return $this->hasMany('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -37,6 +37,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function company () {
+        return $this->belongsTo('App\Company');
+    }
+
     public function isAdmin () {
         if(\Auth::user()->role === 1) {
             return true;
