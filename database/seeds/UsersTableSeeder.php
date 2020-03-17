f97f64bcdc5036596455c5e5189441b30695118e
Fabian Toro <helber.toro@gmail.com>
2020-03-17 11:56:33 -0500

    Create UsersTableSeeder for test

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('users')->insert([
+            'name' => 'Public user',
+            'email' => 'public@test.com',
+            'password' => bcrypt('holahola'),
+            'role' => 2,
+            'company_id' => 1,
+        ]);
+
+        \DB::table('users')->insert([
+            'name' => 'Private user',
+            'email' => 'private@test.com',
+            'password' => bcrypt('holahola'),
+            'role' => 2,
+            'company_id' => 18,
+        ]);
+
+        \DB::table('users')->insert([
+            'name' => 'Admin user',
+            'email' => 'admin@test.com',
+            'password' => bcrypt('holahola'),
+            'role' => 1,
+            'company_id' => 0,
+        ]);
+    }
+}
