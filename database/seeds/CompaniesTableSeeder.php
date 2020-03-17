b30360cf663eb1a0c8dd7f51da48e028b62603ef
Fabian Toro <helber.toro@gmail.com>
2020-03-17 11:21:39 -0500

    Create companies

---
+++ database/seeds/CompaniesTableSeeder.php
@@ -0,0 +1,409 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CompaniesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL CENTRO ORIENTE',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL CHAPINERO',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE BOSA',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE ENGATIVA',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE FONTIBON',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE KENNEDY SEDE PATIO BONITO-TI',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE MEISSEN',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE SUBA',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE TUNJUELITO',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL EL CARMEN',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL EL TUNAL',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL LA VICTORIA- MATERNO INFANTIL',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL LA VICTORIA SEDE PRINCIPAL',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL OCCIDENTE DE KENNEDY',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL SAN BLAS',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL SANTA CLARA',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL SIMÓN BOLÍVAR',
+            'type' => '1'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CENTRO POLICLINICO DEL OLAYA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA COLSUBSIDIO 94',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA COLSUBSIDIO CALLE 100',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA COLSUBSIDIO ORQUIDEAS',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA COLSUBSIDIO ROMA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA DE LA MUJER S.A.S.',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA FUNDADORES',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA FUNDADORES',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLÍNICA GENERAL DE LA 100 S.A.S',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA INFANTIL COLSUBSIDIO',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA INFANTIL COLSUBSIDIO',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA LOS NOGALES',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA MARLY',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA REINA SOFIA - COLSANITAS',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA SAN DIEGO',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLÍNICA SAN RAFAEL',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA UNIVERSITARIA COLOMBIA COLSANITA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'CLINICA VASCULAR NAVARRA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COLSUBSIDIO QUIROGA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR BARRIOS UNIDOS',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR CLINICA EL BOSQUE',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR CLINICA JUAN N CORPAS LTDA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR CLINICA LOS COBOS MEDICAL CENT',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR CLINICA NUEVA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR CLINICA PALERMO',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR CLINICA PARTENON',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR CLINICA UNIVERSIDAD LA SABANA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR HOSPITAL U. MAYOR-MEDERI',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR SEDE AUTOPISTA SUR',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR SEDE CALLE 26',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR SEDE CALLE 94',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR-CLINICA LA ESTANCIA SA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'COMPENSAR-CLINICA MAGDALENA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'E.S.E HOSPITAL LOCAL DE PUERTO ASIS',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'ESE HOSPITAL FRANCISCO VALDERRAMA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'FUND. CARDIO INFANTIL-INST.CARDIOLOGIA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'FUNDACION ABOOD SHAIO',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'FUNDACION HOSPITAL SAN CARLOS',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'ADMINISTRADORA CLINICA LA COLINA SAS',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'ADMINISTRADORA COUNTRY S.A',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'ADMINISTRADORA COUNTRY S.A',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'FUNDACIÓN SANTA FE',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'FUNDACIÓN VALLE DE LILI',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'H. DEPARTAMENTASAN FRANCISCO DE ASIS',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'INSTITUTO NACIONAL DE CANCEROLOGIA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'L. DE CLINICA MARLY DANIEL GAMBOA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'LAB. CLINICO BIOANALISIS H. ORTOPEDICO',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'MIOCARDIO SEDE CASTELLANA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'MIOCARDIO SEDE SUR',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'U+ MOVIL IPS SA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'VIDELMEDICA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'H. DEPARTAMENTASAN FRANCISCO DE ASIS',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE LA MISERICORDIA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE LA POLICIA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE LA POLICIA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DE SAN JOSE',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL DEPARTAMENTAL MARIA INMACULADA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL MILITAR',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL REGIONAL DE CHIQUINQUIRA',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL SAN IGNACIO',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL UNIVERSITARIO DEL VALLE',
+            'type' => '2'
+        ]);
+        
+        \DB::table('companies')->insert([
+            'name' => 'HOSPITAL UNIVERSITARIO NACIONAL DE COLOM',
+            'type' => '2'
+        ]);
+    }
+}
